b88ff92bec617e7060364214cae755ef7283dede
Anas Nadeem <anas5678go@gmail.com>
2023-03-12 19:41:48 +0530

    Updated models, Also added unique_together

--- bookmark_app/models.py
+++ bookmark_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Platform(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     icon = models.ImageField(upload_to='images/', null=True, blank=True)
 
     def __str__(self):
@@ -11,8 +12,11 @@ class Platform(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('name', 'user')
 
     def __str__(self):
         return f"{self.name}"
@@ -21,9 +25,12 @@ class Tag(models.Model):
 class Bookmark(models.Model):
     url = models.URLField()
     title = models.TextField()
-    platform_name = models.ForeignKey(Platform, on_delete=models.SET_NULL, null=True, blank=True)
+    platform = models.ForeignKey(Platform, on_delete=models.SET_NULL, null=True, blank=True)
     tags = models.ManyToManyField(Tag, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ('url', 'user')
+
     def __str__(self):
-        return f"{self.platform_name}: {self.title}"
+        return f"{self.platform}: {self.title}"
