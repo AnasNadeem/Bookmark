4eb326936f0f50cf3a404b233ab8b5461afce408
Anas Nadeem <anas5678go@gmail.com>
2023-03-14 00:11:46 +0530

    URLs added

--- bookmark_app/api/urls.py
+++ bookmark_app/api/urls.py
@@ -1,9 +1,19 @@
 from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
+from .views import (
+    BookmarkViewSet,
+    PlatformViewSet,
+    TagViewSet,
+    UserViewset,
+)
 
 
 router = routers.SimpleRouter(trailing_slash=False)
-# router.register(r"account", AccountViewset, basename="account")
+router.register(r"bookmark", BookmarkViewSet, basename="bookmark")
+router.register(r"platform", PlatformViewSet, basename="platform")
+router.register(r"tag", TagViewSet, basename="tag")
+router.register(r"user", UserViewset, basename="user")
+
 
 urlpatterns = []
 
