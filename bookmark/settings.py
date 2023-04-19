47de45b506df7e29e24bbc4354460014286951c8
Anas Nadeem <anas5678go@gmail.com>
2023-04-19 23:16:43 +0530

    Added chrome extensiones as cors

--- bookmark/settings.py
+++ bookmark/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bookmark.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
+    "chrome-extension://*",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
